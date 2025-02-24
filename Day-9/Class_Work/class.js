028de668d04854f1739cc2bfea6c5ace0171174b
DeveloperSoumya2024 <pandasoumya605@gmail.com>
2025-02-24 11:35:47 +0530

    Started

---
+++ Day-9/Class_Work/class.js
@@ -0,0 +1,53 @@
+// let a = 20
+// a *= 10
+// console.log(a)
+
+// ==, ===, 
+
+// a = 10,b = "10"
+
+// console.log(3+"3"-3)
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+
+// if(a==b){
+//     console.log("a ia equall to b")
+// }else{
+//     console.log("a and b is not equall")
+// }
+
+// let str = "   "
+// let a = str.length
+// console.log(a)
+
+// function myFunction(){
+//     console.log(a+b)
+// }
+
+// myFunction(a=10,b=20)
+// let a = 10
+// let b ="20"
+// const myFunction=(a,b)=>{
+//     console.log(a+b)
+// }
+
+// myFunction(a,b)
+
+
+let a = 10
+let b ="20"
+
+
+console.log(num)  
+
+var num = 10
+
+console.log(num)
+console.log(myFunction)
+
+function myFunction(a,b){
+    console.log(a+b)
+}
+
+myFunction(a,b)
