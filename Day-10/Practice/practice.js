3023ffa05da9ee32e009aa7c9236a984a0fabd10
DeveloperSoumya2024 <pandasoumya605@gmail.com>
2025-02-25 11:29:31 +0530

    PracticeWorkOfDay10

---
+++ Day-10/Practice/practice.js
@@ -0,0 +1,10 @@
+const obj = {
+    "Soumya":{class:"B-Tech",year:3,subjects:["DBMS","IBM"],1:"SVU"},
+    "Panda":{class:"BSc",year:4,subjects:["CS","IBM","RM"]},
+    "Miss":{class:"BCA",year:3,subjects:["Physics","IBM","JD"]},
+    "Aina":{class:"B-Tech",year:2,subjects:["AI","Python","MR"]},
+    "Chacha":{class:"BMLT",year:1,subjects:["Biology","Management","Chemistry"]}
+
+}
+console.log(obj.Soumya)
+
