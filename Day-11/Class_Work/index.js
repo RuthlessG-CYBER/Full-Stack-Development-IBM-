85b0b1711086efdffd4fad859fd3f0625d32d171
DeveloperSoumya2024 <pandasoumya605@gmail.com>
2025-02-25 12:56:17 +0530

    ClassWork

---
+++ Day-11/Class_Work/index.js
@@ -0,0 +1,94 @@
+
+/*
+console.log(this)
+let obj = {
+    name: "Soumya Panda",
+    class: "B-Tech",
+    // myFunction:function(){
+    //     console.log(this);
+    //     console.log(this.name, this.class);
+    //     console.log("I am Method inside Object");  
+    // },
+    myFunction:()=>{
+        // console.log(this);
+        // console.log(this.name, this.class);
+        console.log("I am Method inside Object");  
+    },
+    subjects:["DBMS","AI","JAVA","ANDROID DEV","IOS DEV"],
+    newObj:{
+        1: 11,
+        MyNewFunction: function(){
+            // console.log(this);
+            return this[1]
+        }
+    }
+}
+
+console.log(obj.myFunction);
+obj.newObj.MyNewFunction()
+
+const arrowFunction=()=>{
+    let a=10;
+    let b=20;
+
+    return a+b
+}
+
+let ans = arrowFunction()
+console.log(ans);
+*/
+
+// Condition
+// let num1 = 10
+// let num2 = 5
+
+// if(num1==num2){
+//     console.log("Equall");
+// }else{
+//     console.log("Not");
+// }
+
+// if(num1>num2){
+//     console.log(`${num1} is greater than ${num2}`);
+// }
+// else if(num1==num2){
+//     console.log(`${num1} is equall to ${num2}`);
+// }
+// else{
+//     console.log(`${num1} is not greater than ${num2}`);
+// }
+
+// let suppose you have one num as input you have to check if num is multiple of 3 than print "Fizz", if num is multiple of 5 than print "Buzz". If num is multiple of both 3 and 5 than print "Fizz Buzz"
+// let num = 15
+// a= num%3==0
+// b= num%5==0
+// c= num%(3&&5)==0
+
+
+// if(a && !b){
+//     console.log("Fizz");
+// }else if(b && !a){
+//     console.log("Buzz");   
+// }else if(c){
+//     console.log("Fizz Buzz");
+// }
+
+
+//For Loop
+
+// for (start; condition; increment/decrement){
+
+// }
+
+// let Num = 5
+// for (let i= 1; i <= Num; i++){
+//     console.log(i);
+// }
+
+
+// let arr = [1,2,3,4,5]
+
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i], i)
+// }
+
